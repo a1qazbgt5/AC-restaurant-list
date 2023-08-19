3fef2c8b190f39e71bf72ecd22a6a7900da91de4
a1qazbgt5 <a1995840813@gmail.com>
2023-08-19 14:51:27 +0800

    rearrange ejs and routing

--- app.js
+++ app.js
@@ -6,14 +6,17 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurants })
+  res.redirect('/restaurants')
+})
+
+app.get('/restaurants', (req, res) => {
+  res.render('./layouts/main', { data: { restaurants, body: 'index', title: '我的餐廳總覽' } })
 })
 
 app.get('/restaurants/:id', (req, res) => {
-  const {id} = req.params
+  const { id } = req.params
   const restaurant = restaurants.find(r => r.id.toString() === id)
-  console.log(restaurant)
-  res.render('show', { restaurant })
+  res.render('./layouts/main', { data: { restaurant, body: 'show', title: restaurant.name } })
 })
 
 app.listen(3000, () => {
