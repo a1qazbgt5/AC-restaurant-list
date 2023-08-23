7c01ac1c94b05d0fd4d340fcb04800e67d9d5414
a1qazbgt5 <a1995840813@gmail.com>
2023-08-23 23:20:01 +0800

    implement search function and routing, debug of google map href

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.get('/restaurants', (req, res) => {
   res.render('./layouts/main', { data: { restaurants, body: 'index', title: '我的餐廳總覽' } })
 })
 
+app.get('/restaurants/search', (req, res) => {
+  let { keyword } = req.query
+  keyword = keyword.trim().toLowerCase()
+  const foundRestaurants = restaurants.filter(restaurant =>
+    Object.values(restaurant).some(value => String(value).toLowerCase().includes(keyword)))
+  res.render('./layouts/main', { data: { restaurants: foundRestaurants, body: 'index', title: '符合條件的餐廳' } })
+})
+
 app.get('/restaurants/:id', (req, res) => {
   const { id } = req.params
   const restaurant = restaurants.find(r => r.id.toString() === id)
